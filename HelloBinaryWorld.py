06a907f4869af31004a0a96e9c7a092d0a457e41
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2025-09-10 11:42:29 -0400

    Translate H into binary and print it

--- HelloBinaryWorld.py
+++ HelloBinaryWorld.py
@@ -1,6 +1,9 @@
 #convert 'H' to its number value
+hNumValue = ord('H')
 
 #translate the number value into binary
+hBinValue = bin(hNumValue)[2:]
 
 #print the binary translation
+print(hBinValue)
 
