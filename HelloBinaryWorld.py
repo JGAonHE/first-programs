f666a7f7b74685af8c6e78554727c190784a5dbb
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2025-09-10 11:44:47 -0400

    Finish the translation of all letters and print the binary for Hello

--- HelloBinaryWorld.py
+++ HelloBinaryWorld.py
@@ -10,6 +10,18 @@ eNumValue = ord('e')
 #translate the  number value into binary
 eBinValue = bin(eNumValue)[2:]
 
+#convert 'l' to its number value
+lNumValue = ord('l')
+
+#translate the number value into binary
+lBinValue = bin(lNumValue)[2:]
+
+#convert 'o' to its number value
+oNumValue = ord('o')
+
+#translate the numver value into binary
+oBinValue = bin(oNumValue)[2:]
+
 #print the binary translation
-print(hBinValue, eBinValue)
+print(hBinValue, eBinValue, lBinValue, lBinValue, oBinValue)
 
