09fec359e4d6f8e56e2a92816ff15268691e3dc0
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2025-09-10 11:24:24 -0400

    Ask for three pieces of input from ther user: subject, course name and course schedule.

--- InputFormat.py
+++ InputFormat.py
@@ -1,4 +1,7 @@
 #Prompt the user for input: subject, course and schedule
+subject = input('What subject do you study? ')
+courseName = input('What is the name of the course?')
+courseSchedule = input('When is your course?')
 
 #Format a string using the input from the user, mixing literal characters with program variables
 
