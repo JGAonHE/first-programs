ebe890d9bb2c77326c003516a794e1c2fbc31d5b
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2025-09-10 11:43:49 -0400

    Repeat the process for the letter 'e'

--- HelloBinaryWorld.py
+++ HelloBinaryWorld.py
@@ -4,6 +4,12 @@ hNumValue = ord('H')
 #translate the number value into binary
 hBinValue = bin(hNumValue)[2:]
 
+#convert 'e' to its number value
+eNumValue = ord('e')
+
+#translate the  number value into binary
+eBinValue = bin(eNumValue)[2:]
+
 #print the binary translation
-print(hBinValue)
+print(hBinValue, eBinValue)
 
