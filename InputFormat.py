789e3f0e1da8958b9982d4ff8656a63db48a80f4
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2025-09-10 11:23:47 -0400

    Plan the development of the program with comments. Problems should be solved in natural language (e.g. English)

---
+++ InputFormat.py
@@ -0,0 +1,7 @@
+#Prompt the user for input: subject, course and schedule
+
+#Format a string using the input from the user, mixing literal characters with program variables
+
+#Present the three messages to the user: hello, course message, see you message
+ 
+
