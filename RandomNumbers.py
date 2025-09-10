b484b9fad8ded86051e37335c543b080b72f7cda
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2025-09-10 11:54:46 -0400

    Generate and print a random integer between 1 and 10.

--- RandomNumbers.py
+++ RandomNumbers.py
@@ -4,3 +4,7 @@ import random
 #create and print a random number
 randNum = random.random()
 print(f"A random number is {randNum}")
+
+#create and print a random integer from 1 to 10
+randGuess = random.randint(1, 10)
+print(f"A random integer between 1 and 10 is {randGuess}")
