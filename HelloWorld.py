ade40204b3c2e35c3212c5e72e6dcc9fa3be9da4
komi <wqeqq543@gmail.com>
2025-11-02 18:33:53 -0500

    Created the simples program.

---
+++ HelloWorld.py
@@ -0,0 +1,6 @@
+#say hello to the user with a message
+print("Hello, Programming principles, Fall 2025")
+
+#add another message. Python is ....
+print("Python is ....fun!")
+
---
+++ SimplestProgram.py
@@ -0,0 +1,2 @@
+#The simplest program in Python is an empty flie.This is a comment.
+
