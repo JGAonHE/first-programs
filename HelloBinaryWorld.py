c36b00708d709222239249bc942ff1916b627280
komi <wqeqq543@gmail.com>
2025-11-02 21:36:48 -0500

    changes

---
+++ HelloBinaryWorld.py
@@ -0,0 +1,12 @@
+#covert H INTO a number, it's Ascii code
+hNumvalue = ord ('H')
+
+#convert the code for H into binary
+hBinValue = bin (hNumvalue)[2:]
+
+#print the binary for H
+print (hBinValue)
+
+
+
+
