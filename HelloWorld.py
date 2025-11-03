f8504d769cddf072b2ab8cc5613766d815162e24
komi <wqeqq543@gmail.com>
2025-11-02 20:17:20 -0500

    create a interactiveHelloworld says hello to user

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,6 +1,4 @@
 #say hello to the user with a message
 print("Hello, Programming principles, Fall 2025")
 
-#add another message. Python is ....
-print("Python is ....fun!")
 
---
+++ InteractiveHelloWorld.py
@@ -0,0 +1,9 @@
+#ask the user what their name is
+userName = input("What is your name? ")
+
+#compose a greeting that includes their name
+greeting = f"Hello {userName}!"
+
+#print the greeting
+print(greeting)
+
