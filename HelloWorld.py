22ab5dd0f81530c03cd923a7a963946cfe910c1b
komi <wqeqq543@gmail.com>
2025-11-02 21:02:51 -0500

    change

---
+++ HelloWorld.py
@@ -0,0 +1,4 @@
+#say hello to the user with a message
+print("Hello, Programming principles, Fall 2025")
+
+
---
+++ InteractiveHelloWorld.py
@@ -0,0 +1,13 @@
+#ask the user what their name is
+userName = input("What is your name? ")
+
+#compose a greeting that includes their name
+greeting = f"Hello {userName}!"
+
+#print the greeting
+print(greeting)
+
+#prvent the program for finishing until the user press enter
+input("Press Enter to end the program")
+
+
