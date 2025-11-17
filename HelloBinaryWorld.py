2c9856a809e68ea99eafbaa6fe9fe5f636a546f4
komikado <wqeqq543@gmail.com>
2025-11-17 12:36:47 -0500

    change

--- HelloBinaryWorld.py
+++ HelloBinaryWorld.py
@@ -2,10 +2,32 @@
 hNumvalue = ord ('H')
 
 #convert the code for H into binary
-hBinValue = bin (hNumvalue)[2:]
+hBinValue = bin (hNumvalue) [2:]
 
 #print the binary for H
 print (hBinValue)
+
+#convert e into a number
+eNumValue = ord ('e')
+
+#conver the number for e into binary
+eBinValue = bin (eNumValue) [2:]
+
+#convert l into a number
+lNumValue = ord ('l')
+
+#conver the number for l into binary
+lBinValue = bin (eNumValue) [2:]
+
+#convert o into a number
+oNumValue = ord ('o')
+
+#conver the number for o into binary
+oBinValue = bin (oNumValue) [2:]
+
+#print the binary translation of Hello world
+print (hBinValue, eBinValue, lBinValue, oBinValue)
+
 
 
 
